1cd791968b16f5f1c73f1531ad317d47a508b967
fredo <frederic.testard@gmail.com>
2015-01-07 18:17:38 +0100

    started adapting to new ReverseDiffSource

--- src/parsers/definitions/AccumulatorDerivRules.jl
+++ src/parsers/definitions/AccumulatorDerivRules.jl
@@ -19,14 +19,19 @@ end
 +(ll::LLAcc, x::Real)           = LLAcc(ll.val + x)
 +(ll::LLAcc, x::Array{Float64}) = LLAcc(ll.val + sum(x))
 
-ReverseDiffSource.declareType(Lora.LLAcc, :LLAcc) # declares new type to Autodiff
+# ReverseDiffSource.declareType(LLAcc, :LLAcc) # declares new type to Autodiff
+typeequiv(LLAcc, 1)
 
 ####### derivation rules  ############
 # (note : only additions are possible with LLAcc type )
 # ReverseDiffSource.@deriv_rule getfield(x::LLAcc, f )  x  dx1 = ds
-ReverseDiffSource.@deriv_rule LLAcc(x::Real) x  ds[1]
+@deriv_rule LLAcc(x::Real)      x      ds[1]
 
-ReverseDiffSource.@deriv_rule   +(x::LLAcc,   y)                  x   1. * ds
-ReverseDiffSource.@deriv_rule   +(x::LLAcc,   y::Real)            y   ds[1]
-ReverseDiffSource.@deriv_rule   +(x::LLAcc,   y::AbstractArray)   y   fill(ds[1], size(y))
+# ReverseDiffSource.@deriv_rule   +(x::LLAcc,   y)                  x   1. * ds
+# ReverseDiffSource.@deriv_rule   +(x::LLAcc,   y::Real)            y   ds[1]
+# ReverseDiffSource.@deriv_rule   +(x::LLAcc,   y::AbstractArray)   y   fill(ds[1], size(y))
+# @deriv_rule +(x::LLAcc, y)                  x   1. * ds
+@deriv_rule +(x::LLAcc, y::Real)            y   ds[1]
+@deriv_rule +(x::LLAcc, y::AbstractArray)   y   fill(ds[1], size(y))
 
+#ReverseDiffSource.@deriv_rule   getfield(x::LLAcc, f)             x   ds[1]
