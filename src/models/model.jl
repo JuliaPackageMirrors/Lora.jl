fb589a4dc558fbd323dbe42543459b6389785d0f
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2015-05-22 12:50:48 +0100

    Added constructors for Model graph

--- src/models/model.jl
+++ src/models/model.jl
@@ -1,1 +1,5 @@
 typealias Model GenericGraph{Variable, Dependence, Vector{Variable}, Vector{Dependence}, Vector{Vector{Dependence}}}
+
+Model(vs::Vector{Variable}, ds::Vector{Dependence}; is_directed::Bool=true) = graph(vs, ds, is_directed=is_directed)
+
+Model(is_directed::Bool=true) = graph(Variable[], Dependence[], is_directed=is_directed)
