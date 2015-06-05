b15a2dcc8195199db3a5d67bbe46e583670e7292
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2015-06-05 23:27:02 +0100

    pdf now a field in parameter (rather than its state)

--- src/variables/parameters.jl
+++ src/variables/parameters.jl
@@ -40,7 +40,6 @@ ContinuousUnivariateParameterState{N<:FloatingPoint}(::Type{N}=Float64) =
 
 type ContinuousMultivariateParameterState{N<:FloatingPoint} <: ParameterState{Continuous, Multivariate, N}
   value::Vector{N}
-  pdf::Union(ContinuousMultivariateDistribution, Nothing)
   loglikelihood::N
   logprior::N
   logtarget::N
@@ -52,11 +51,7 @@ type ContinuousMultivariateParameterState{N<:FloatingPoint} <: ParameterState{Co
   size::Int
 end
 
-function ContinuousMultivariateParameterState{N<:FloatingPoint}(
-  value::Vector{N},
-  pdf::Union(ContinuousMultivariateDistribution, Nothing)=nothing,
-  monitor::Vector{Bool}=fill(false, 5)
-)
+function ContinuousMultivariateParameterState{N<:FloatingPoint}(value::Vector{N}, monitor::Vector{Bool}=fill(false, 5))
   size::Int = length(value)
 
   l::Vector{Int} = Array(Int, 5)
@@ -66,7 +61,6 @@ function ContinuousMultivariateParameterState{N<:FloatingPoint}(
 
   ContinuousMultivariateParameterState{N}(
     value,
-    pdf,
     convert(N, NaN),
     convert(N, NaN),
     convert(N, NaN),
@@ -81,7 +75,6 @@ end
 
 function ContinuousMultivariateParameterState{N<:FloatingPoint}(
   ::Type{N}=Float64,
-  pdf::Union(ContinuousMultivariateDistribution, Nothing)=nothing,
   size::Int=0,
   monitor::Vector{Bool}=[true, fill(false, 6)]
   )
@@ -93,7 +86,6 @@ function ContinuousMultivariateParameterState{N<:FloatingPoint}(
 
   ContinuousMultivariateParameterState{N}(
     Array(N, l[1]),
-    pdf,
     convert(N, NaN),
     convert(N, NaN),
     convert(N, NaN),
@@ -117,7 +109,7 @@ type ContinuousUnivariateParameter{N<:FloatingPoint} <: Parameter{Continuous, Un
   index::Int
   key::Symbol
   pdf::Union(ContinuousUnivariateDistribution, Nothing)
-  setpdf!::Union(Function, Nothing)
+  setpdf::Union(Function, Nothing)
   loglikelihood::Union(Function, Nothing)
   logprior::Union(Function, Nothing)
   logtarget::Union(Function, Nothing)
@@ -140,7 +132,7 @@ type ContinuousUnivariateParameter{N<:FloatingPoint} <: Parameter{Continuous, Un
     index::Int,
     key::Symbol,
     pdf::Union(ContinuousUnivariateDistribution, Nothing),
-    setpdf!::Union(Function, Nothing),
+    setpdf::Union(Function, Nothing),
     ll::Union(Function, Nothing),
     lp::Union(Function, Nothing),
     lt::Union(Function, Nothing),
@@ -164,9 +156,9 @@ type ContinuousUnivariateParameter{N<:FloatingPoint} <: Parameter{Continuous, Un
     instance.key = key
     instance.pdf = pdf
 
-    fin = (setpdf!, ll, lp, lt, gll, glp, glt, tll, tlp, tlt, dtll, dtlp, dtlt, uptoglt, uptotlt, uptodtlt, rand)
+    fin = (setpdf, ll, lp, lt, gll, glp, glt, tll, tlp, tlt, dtll, dtlp, dtlt, uptoglt, uptotlt, uptodtlt, rand)
     fnames = (
-      "setpdf!",
+      "setpdf",
       "loglikelihood",
       "logprior",
       "logtarget",
@@ -209,7 +201,7 @@ type ContinuousUnivariateParameter{N<:FloatingPoint} <: Parameter{Continuous, Un
         elseif isa(fin[1], Function)
           fout[i] =
             (pstate::ContinuousUnivariateParameterState{N}, nstate::Dict{Variable, VariableState}) ->
-            f(setpdf!(pstate, nstate), pstate.value)
+            f(setpdf(pstate, nstate), pstate.value)
         elseif isa(instance.pdf, ContinuousUnivariateDistribution) && method_exists(f, (typeof(instance.pdf), N))
           fout[i] =
             (pstate::ContinuousUnivariateParameterState{N}, nstate::Dict{Variable, VariableState}) ->
@@ -257,7 +249,7 @@ type ContinuousUnivariateParameter{N<:FloatingPoint} <: Parameter{Continuous, Un
       if isa(fin[1], Function)
         fout[17] =
           function (pstate::ContinuousUnivariateParameterState{N}, nstate::Dict{Variable, VariableState})
-          instance.pdf = setpdf!(pstate, nstate)
+          instance.pdf = setpdf(pstate, nstate)
           Distributions.rand(instance.pdf)
         end
       elseif isa(instance.pdf, ContinuousUnivariateDistribution) &&
@@ -268,7 +260,7 @@ type ContinuousUnivariateParameter{N<:FloatingPoint} <: Parameter{Continuous, Un
       end
     end
 
-    instance.setpdf! = fout[1]
+    instance.setpdf = fout[1]
     instance.loglikelihood = fout[2]
     instance.logprior = fout[3]
     instance.logtarget = fout[4]
@@ -295,7 +287,7 @@ function ContinuousUnivariateParameter{N<:FloatingPoint}(
   index::Int,
   key::Symbol,
   pdf::Union(ContinuousUnivariateDistribution, Nothing)=nothing,
-  setpdf!::Union(Function, Nothing)=nothing,
+  setpdf::Union(Function, Nothing)=nothing,
   loglikelihood::Union(Function, Nothing)=nothing,
   logprior::Union(Function, Nothing)=nothing,
   logtarget::Union(Function, Nothing)=nothing,
@@ -318,7 +310,7 @@ function ContinuousUnivariateParameter{N<:FloatingPoint}(
     index,
     key,
     pdf,
-    setpdf!,
+    setpdf,
     loglikelihood,
     logprior,
     logtarget,
@@ -343,7 +335,7 @@ function ContinuousUnivariateParameter{N<:FloatingPoint}(
   index::Int,
   key::Symbol;
   pdf::Union(ContinuousUnivariateDistribution, Nothing)=nothing,
-  setpdf!::Union(Function, Nothing)=nothing,
+  setpdf::Union(Function, Nothing)=nothing,
   loglikelihood::Union(Function, Nothing)=nothing,
   logprior::Union(Function, Nothing)=nothing,
   logtarget::Union(Function, Nothing)=nothing,
@@ -366,7 +358,7 @@ function ContinuousUnivariateParameter{N<:FloatingPoint}(
     index,
     key,
     pdf,
-    setpdf!,
+    setpdf,
     loglikelihood,
     logprior,
     logtarget,
@@ -390,7 +382,8 @@ end
 type ContinuousMultivariateParameter{N<:FloatingPoint} <: Parameter{Continuous, Multivariate, N}
   index::Int
   key::Symbol
-  setpdf!::Union(Function, Nothing)
+  pdf::Union(ContinuousMultivariateDistribution, Nothing)
+  setpdf::Union(Function, Nothing)
   loglikelihood::Union(Function, Nothing)
   logprior::Union(Function, Nothing)
   logtarget::Union(Function, Nothing)
@@ -408,159 +401,167 @@ type ContinuousMultivariateParameter{N<:FloatingPoint} <: Parameter{Continuous, 
   uptodtensorlogtarget::Union(Function, Nothing)
   rand::Union(Function, Nothing)
   state::ContinuousMultivariateParameterState{N}
+
+  ContinuousMultivariateParameter(
+    index::Int,
+    key::Symbol,
+    pdf::Union(ContinuousMultivariateDistribution, Nothing),
+    setpdf::Union(Function, Nothing),
+    ll::Union(Function, Nothing),
+    lp::Union(Function, Nothing),
+    lt::Union(Function, Nothing),
+    gll::Union(Function, Nothing),
+    glp::Union(Function, Nothing),
+    glt::Union(Function, Nothing),
+    tll::Union(Function, Nothing),
+    tlp::Union(Function, Nothing),
+    tlt::Union(Function, Nothing),
+    dtll::Union(Function, Nothing),
+    dtlp::Union(Function, Nothing),
+    dtlt::Union(Function, Nothing),
+    uptoglt::Union(Function, Nothing),
+    uptotlt::Union(Function, Nothing),
+    uptodtlt::Union(Function, Nothing),
+    rand::Union(Function, Nothing),
+    state::ContinuousMultivariateParameterState{N}
+  ) = begin
+    instance = new()
+    instance.index = index
+    instance.key = key
+    instance.pdf = pdf
+
+    fin = (setpdf, ll, lp, lt, gll, glp, glt, tll, tlp, tlt, dtll, dtlp, dtlt, uptoglt, uptotlt, uptodtlt, rand)
+    fnames = (
+      "setpdf",
+      "loglikelihood",
+      "logprior",
+      "logtarget",
+      "gradloglikelihood",
+      "gradlogprior",
+      "gradlogtarget",
+      "tensorloglikelihood",
+      "tensorlogprior",
+      "tensorlogtarget",
+      "dtensorloglikelihood",
+      "dtensorlogprior",
+      "dtensorlogtarget",
+      "uptogradlogtarget",
+      "uptotensorlogtarget",
+      "uptodtensorlogtarget",
+      "rand"
+    )
+    nf = 17
+
+    # Check that all generic functions have correct signature
+    for i = 1:nf
+      if isa(fin[i], Function) &&
+        isgeneric(fin[i]) &&
+        !method_exists(fin[i], (ContinuousMultivariateParameterState{N}, Dict{Variable, VariableState}))
+        error("$(fnames[i]) has wrong signature")
+      end
+    end
+
+    # Initialize output vector of functions fout by copying into it contents of fin
+    fout = Union(Function, Nothing)[fin[i] for i in 1:nf]
+
+    # Define logtarget (i = 4) and gradlogtarget (i = 7)
+    for (i , f) in ((4, logpdf), (7, gradlogpdf))
+      if fin[i] == nothing
+        if isa(fin[i-2], Function) && isa(fin[i-1], Function)
+          # pstate and nstate stand for parameter state and neighbors' state respectively
+          fout[i] =
+            (pstate::ContinuousMultivariateParameterState{N}, nstate::Dict{Variable, VariableState}) ->
+            fin[i-2](pstate, nstate)+fin[i-1](pstate, nstate)
+        elseif isa(fin[1], Function)
+          fout[i] =
+            (pstate::ContinuousMultivariateParameterState{N}, nstate::Dict{Variable, VariableState}) ->
+            f(setpdf(pstate, nstate), pstate.value)
+        elseif isa(instance.pdf, ContinuousMultivariateDistribution) && method_exists(f, (typeof(instance.pdf), N))
+          fout[i] =
+            (pstate::ContinuousMultivariateParameterState{N}, nstate::Dict{Variable, VariableState}) ->
+            f(instance.pdf, pstate.value)
+        end
+      end
+    end
+
+    # Define tensorlogtarget (i = 10) and dtensorlogtarget (i = 13)
+    for i in (10, 13)
+      if fin[i] == nothing && isa(fin[i-2], Function) && isa(fin[i-1], Function)
+        fout[i] =
+          (pstate::ContinuousMultivariateParameterState{N}, nstate::Dict{Variable, VariableState}) ->
+          fin[i-2](pstate, nstate)+fin[i-1](pstate, nstate)
+      end
+    end
+
+    # Define uptogradlogtarget
+    if fin[14] == nothing && isa(fout[4], Function) && isa(fout[7], Function)
+      fout[14] =
+        (pstate::ContinuousMultivariateParameterState{N}, nstate::Dict{Variable, VariableState}) ->
+        (fout[4](pstate, nstate), fout[7](pstate, nstate))
+    end
+
+    # Define uptotensorlogtarget
+    if fin[15] == nothing && isa(fout[4], Function) && isa(fout[7], Function) && isa(fout[10], Function)
+      fout[15] =
+        (pstate::ContinuousMultivariateParameterState{N}, nstate::Dict{Variable, VariableState}) ->
+        (fout[4](pstate, nstate), fout[7](pstate, nstate), fout[10](pstate, nstate))
+    end
+
+    # Define uptodtensorlogtarget
+    if fin[16] == nothing &&
+      isa(fout[4], Function) &&
+      isa(fout[7], Function) &&
+      isa(fout[10], Function) &&
+      isa(fout[13], Function)
+      fout[16] =
+        (pstate::ContinuousMultivariateParameterState{N}, nstate::Dict{Variable, VariableState}) ->
+        (fout[4](pstate, nstate), fout[7](pstate, nstate), fout[10](pstate, nstate), fout[13](pstate, nstate))
+    end
+
+    # Define rand
+    if fin[17] == nothing
+      if isa(fin[1], Function)
+        fout[17] =
+          function (pstate::ContinuousMultivariateParameterState{N}, nstate::Dict{Variable, VariableState})
+          instance.pdf = setpdf(pstate, nstate)
+          Distributions.rand(instance.pdf)
+        end
+      elseif isa(instance.pdf, ContinuousMultivariateDistribution) &&
+        method_exists(Distributions.rand, (typeof(instance.pdf),))
+        fout[17] =
+          (pstate::ContinuousMultivariateParameterState{N}, nstate::Dict{Variable, VariableState}) ->
+          Distributions.rand(pdf)
+      end
+    end
+
+    instance.setpdf = fout[1]
+    instance.loglikelihood = fout[2]
+    instance.logprior = fout[3]
+    instance.logtarget = fout[4]
+    instance.gradloglikelihood = fout[5]
+    instance.gradlogprior = fout[6]
+    instance.gradlogtarget = fout[7]
+    instance.tensorloglikelihood = fout[8]
+    instance.tensorlogprior = fout[9]
+    instance.tensorlogtarget = fout[10]
+    instance.dtensorloglikelihood = fout[11]
+    instance.dtensorlogprior = fout[12]
+    instance.dtensorlogtarget = fout[13]
+    instance.uptogradlogtarget = fout[14]
+    instance.uptotensorlogtarget = fout[15]
+    instance.uptodtensorlogtarget = fout[16]
+    instance.rand = fout[17]
+    instance.state = state
+
+    instance
+  end
 end
 
 function ContinuousMultivariateParameter{N<:FloatingPoint}(
   index::Int,
   key::Symbol,
-  setpdf!::Union(Function, Nothing),
-  ll::Union(Function, Nothing),
-  lp::Union(Function, Nothing),
-  lt::Union(Function, Nothing),
-  gll::Union(Function, Nothing),
-  glp::Union(Function, Nothing),
-  glt::Union(Function, Nothing),
-  tll::Union(Function, Nothing),
-  tlp::Union(Function, Nothing),
-  tlt::Union(Function, Nothing),
-  dtll::Union(Function, Nothing),
-  dtlp::Union(Function, Nothing),
-  dtlt::Union(Function, Nothing),
-  uptoglt::Union(Function, Nothing),
-  uptotlt::Union(Function, Nothing),
-  uptodtlt::Union(Function, Nothing),
-  rand::Union(Function, Nothing),
-  state::ContinuousMultivariateParameterState{N}
-)
-  fin = (setpdf!, ll, lp, lt, gll, glp, glt, tll, tlp, tlt, dtll, dtlp, dtlt, uptoglt, uptotlt, uptodtlt, rand)
-  fnames = (
-    "setpdf!",
-    "loglikelihood",
-    "logprior",
-    "logtarget",
-    "gradloglikelihood",
-    "gradlogprior",
-    "gradlogtarget",
-    "tensorloglikelihood",
-    "tensorlogprior",
-    "tensorlogtarget",
-    "dtensorloglikelihood",
-    "dtensorlogprior",
-    "dtensorlogtarget",
-    "uptogradlogtarget",
-    "uptotensorlogtarget",
-    "uptodtensorlogtarget",
-    "rand"
-  )
-  nf = 17
-
-  # Check that all generic functions have correct signature
-  for i = 1:nf
-    if isa(fin[i], Function) &&
-      isgeneric(fin[i]) &&
-      !method_exists(fin[i], (ContinuousMultivariateParameterState{N}, Dict{Variable, VariableState}))
-      error("$(fnames[i]) has wrong signature")
-    end
-  end
-
-  # Initialize output vector of functions fout by copying into it contents of fin
-  fout = Union(Function, Nothing)[fin[i] for i in 1:nf]
-
-  # Define logtarget (i = 4) and gradlogtarget (i = 7)
-  for (i , f) in ((4, logpdf), (7, gradlogpdf))
-    if fin[i] == nothing
-      if isa(fin[i-2], Function) && isa(fin[i-1], Function)
-        # pstate and nstate stand for parameter state and neighbors' state respectively
-        fout[i] =
-          (pstate::ContinuousMultivariateParameterState{N}, nstate::Dict{Variable, VariableState}) ->
-          fin[i-2](pstate, nstate)+fin[i-1](pstate, nstate)
-      elseif isa(fin[1], Function)
-        fout[i] =
-          (pstate::ContinuousMultivariateParameterState{N}, nstate::Dict{Variable, VariableState}) ->
-          f(setpdf!(pstate, nstate), pstate.value)
-      elseif isa(state.pdf, ContinuousUnivariateDistribution) && method_exists(f, (typeof(state.pdf), N))
-        fout[i] =
-          (pstate::ContinuousMultivariateParameterState{N}, nstate::Dict{Variable, VariableState}) ->
-          f(pstate.pdf, pstate.value)
-      end
-    end
-  end
-
-  # Define tensorlogtarget (i = 10) and dtensorlogtarget (i = 13)
-  for i in (10, 13)
-    if fin[i] == nothing && isa(fin[i-2], Function) && isa(fin[i-1], Function)
-      fout[i] =
-        (pstate::ContinuousMultivariateParameterState{N}, nstate::Dict{Variable, VariableState}) ->
-        fin[i-2](pstate, nstate)+fin[i-1](pstate, nstate)
-    end
-  end
-
-  # Define uptogradlogtarget
-  if fin[14] == nothing && isa(fout[4], Function) && isa(fout[7], Function)
-    fout[14] =
-      (pstate::ContinuousMultivariateParameterState{N}, nstate::Dict{Variable, VariableState}) ->
-      (fout[4](pstate, nstate), fout[7](pstate, nstate))
-  end
-
-  # Define uptotensorlogtarget
-  if fin[15] == nothing && isa(fout[4], Function) && isa(fout[7], Function) && isa(fout[10], Function)
-    fout[15] =
-      (pstate::ContinuousMultivariateParameterState{N}, nstate::Dict{Variable, VariableState}) ->
-      (fout[4](pstate, nstate), fout[7](pstate, nstate), fout[10](pstate, nstate))
-  end
-
-  # Define uptodtensorlogtarget
-  if fin[16] == nothing &&
-    isa(fout[4], Function) &&
-    isa(fout[7], Function) &&
-    isa(fout[10], Function) &&
-    isa(fout[13], Function)
-    fout[16] =
-      (pstate::ContinuousMultivariateParameterState{N}, nstate::Dict{Variable, VariableState}) ->
-      (fout[4](pstate, nstate), fout[7](pstate, nstate), fout[10](pstate, nstate), fout[13](pstate, nstate))
-  end
-
-  # Define rand
-  if fin[17] == nothing
-    if isa(fin[1], Function)
-      fout[17] =
-        (pstate::ContinuousMultivariateParameterState{N}, nstate::Dict{Variable, VariableState}, n::Int) ->
-        Distributions.rand(setpdf!(pstate, nstate), n)
-    elseif isa(state.pdf, ContinuousUnivariateDistribution) && method_exists(Distributions.rand, (typeof(state.pdf),))
-      fout[17] =
-        (pstate::ContinuousMultivariateParameterState{N}, nstate::Dict{Variable, VariableState}, n::Int) ->
-        Distributions.rand(pstate.pdf, n)
-    end
-  end
-
-  ContinuousMultivariateParameter{N}(
-    index,
-    key,
-    fout[1],
-    fout[2],
-    fout[3],
-    fout[4],
-    fout[5],
-    fout[6],
-    fout[7],
-    fout[8],
-    fout[9],
-    fout[10],
-    fout[11],
-    fout[12],
-    fout[13],
-    fout[14],
-    fout[15],
-    fout[16],
-    fout[17],
-    state
-  )
-end
-
-function ContinuousMultivariateParameter{N<:FloatingPoint}(
-  index::Int,
-  key::Symbol;
-  setpdf!::Union(Function, Nothing)=nothing,
+  pdf::Union(ContinuousMultivariateDistribution, Nothing)=nothing,
+  setpdf::Union(Function, Nothing)=nothing,
   loglikelihood::Union(Function, Nothing)=nothing,
   logprior::Union(Function, Nothing)=nothing,
   logtarget::Union(Function, Nothing)=nothing,
@@ -582,7 +583,56 @@ function ContinuousMultivariateParameter{N<:FloatingPoint}(
   ContinuousMultivariateParameter{N}(
     index,
     key,
-    setpdf!,
+    pdf,
+    setpdf,
+    loglikelihood,
+    logprior,
+    logtarget,
+    gradloglikelihood,
+    gradlogprior,
+    gradlogtarget,
+    tensorloglikelihood,
+    tensorlogprior,
+    tensorlogtarget,
+    dtensorloglikelihood,
+    dtensorlogprior,
+    dtensorlogtarget,
+    uptogradlogtarget,
+    uptotensorlogtarget,
+    uptodtensorlogtarget,
+    rand,
+    state
+  )
+end
+
+function ContinuousMultivariateParameter{N<:FloatingPoint}(
+  index::Int,
+  key::Symbol;
+  pdf::Union(ContinuousMultivariateDistribution, Nothing)=nothing,
+  setpdf::Union(Function, Nothing)=nothing,
+  loglikelihood::Union(Function, Nothing)=nothing,
+  logprior::Union(Function, Nothing)=nothing,
+  logtarget::Union(Function, Nothing)=nothing,
+  gradloglikelihood::Union(Function, Nothing)=nothing,
+  gradlogprior::Union(Function, Nothing)=nothing,
+  gradlogtarget::Union(Function, Nothing)=nothing,
+  tensorloglikelihood::Union(Function, Nothing)=nothing,
+  tensorlogprior::Union(Function, Nothing)=nothing,
+  tensorlogtarget::Union(Function, Nothing)=nothing,
+  dtensorloglikelihood::Union(Function, Nothing)=nothing,
+  dtensorlogprior::Union(Function, Nothing)=nothing,
+  dtensorlogtarget::Union(Function, Nothing)=nothing,
+  uptogradlogtarget::Union(Function, Nothing)=nothing,
+  uptotensorlogtarget::Union(Function, Nothing)=nothing,
+  uptodtensorlogtarget::Union(Function, Nothing)=nothing,
+  rand::Union(Function, Nothing)=nothing,
+  state::ContinuousMultivariateParameterState{N}=ContinuousMultivariateParameterState(Float64)
+)
+  ContinuousMultivariateParameter{N}(
+    index,
+    key,
+    pdf,
+    setpdf,
     loglikelihood,
     logprior,
     logtarget,
--- test/parameters.jl
+++ test/parameters.jl
@@ -13,17 +13,16 @@ println("    Testing basic ContinuousUnivariateParameter constructors...")
 ContinuousUnivariateParameter(1, :p)
 ContinuousUnivariateParameter(1, :p, pdf=Normal(), state=ContinuousUnivariateParameterState(Float32))
 
-#=
+
 println("    Testing basic ContinuousMultivariateParameterState constructors...")
 
 ContinuousMultivariateParameterState([1., 1.5])
 ContinuousMultivariateParameterState(Float32)
 ContinuousMultivariateParameterState(Float64)
 ContinuousMultivariateParameterState(BigFloat)
-ContinuousMultivariateParameterState(Float64, MvNormal(ones(2)))
 
 println("    Testing basic ContinuousMultivariateParameter constructors...")
 
 ContinuousMultivariateParameter(1, :p)
 ContinuousMultivariateParameter(1, :p, pdf=MvNormal(ones(2)), state=ContinuousMultivariateParameterState(Float32))
-=#
+
