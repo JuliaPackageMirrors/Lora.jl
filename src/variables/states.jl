93f2ef21bdd7608ccf0b1aaca02c29e7552e91f7
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2015-07-15 10:13:13 +0100

    Further refinements in parameter state constructors

--- src/variables/states.jl
+++ src/variables/states.jl
@@ -48,23 +48,40 @@ type ContinuousUnivariateParameterState{N<:FloatingPoint} <: ParameterState{Univ
   gradlogtarget::N
   tensorlogtarget::N
   dtensorlogtarget::N
+  diagnostics::Dict
 end
 
-ContinuousUnivariateParameterState{N<:FloatingPoint}(value::N) =
+function ContinuousUnivariateParameterState{N<:FloatingPoint}(value::N)
+  v = convert(N, NaN)
+  ContinuousUnivariateParameterState{N}(value, v, v, v, v, v, v, v, v, Dict())
+end
+
+ContinuousUnivariateParameterState{N<:FloatingPoint}(::Type{N}) = ContinuousUnivariateParameterState(convert(N, NaN))
+
+ContinuousUnivariateParameterState{N<:FloatingPoint}(;
+  value::N = convert(N, NaN),
+  loglikelihood::N=convert(N, NaN),
+  logprior::N=convert(N, NaN),
+  logtarget::N=convert(N, NaN),
+  gradloglikelihood::N=convert(N, NaN),
+  gradlogprior::N=convert(N, NaN),
+  gradlogtarget::N=convert(N, NaN),
+  tensorlogtarget::N=convert(N, NaN),
+  dtensorlogtarget::N=convert(N, NaN),
+  diagnostics::Dict=Dict()
+) =
   ContinuousUnivariateParameterState{N}(
     value,
-    convert(N, NaN),
-    convert(N, NaN),
-    convert(N, NaN),
-    convert(N, NaN),
-    convert(N, NaN),
-    convert(N, NaN),
-    convert(N, NaN),
-    convert(N, NaN)
+    loglikelihood,
+    logprior,
+    logtarget,
+    gradloglikelihood,
+    gradlogprior,
+    gradlogtarget,
+    tensorlogtarget,
+    dtensorlogtarget,
+    diagnostics
   )
-
-ContinuousUnivariateParameterState{N<:FloatingPoint}(::Type{N}=Float64) =
-  ContinuousUnivariateParameterState(convert(N, NaN))
 
 ## ContinuousMultivariateParameterState
 
@@ -79,6 +96,7 @@ type ContinuousMultivariateParameterState{N<:FloatingPoint} <: ParameterState{Mu
   tensorlogtarget::Matrix{N}
   dtensorlogtarget::Array{N, 3}
   size::Int
+  diagnostics::Dict
 end
 
 function ContinuousMultivariateParameterState{N<:FloatingPoint}(value::Vector{N}, monitor::Vector{Bool}=fill(false, 5))
@@ -89,40 +107,60 @@ function ContinuousMultivariateParameterState{N<:FloatingPoint}(value::Vector{N}
     l[i] = (monitor[i] == false ? zero(Int) : size)
   end
 
+  v = convert(N, NaN)
+
   ContinuousMultivariateParameterState{N}(
     value,
-    convert(N, NaN),
-    convert(N, NaN),
-    convert(N, NaN),
+    v,
+    v,
+    v,
     Array(N, l[1]),
     Array(N, l[2]),
     Array(N, l[3]),
     Array(N, l[4], l[4]),
     Array(N, l[5], l[5], l[5]),
-    size
+    size,
+    Dict()
   )
 end
 
 function ContinuousMultivariateParameterState{N<:FloatingPoint}(value::Vector{N}, monitor::Dict{Symbol, Bool})
-  fields =  (:gradloglikelihood, :gradlogprior, :gradlogtarget, :tensorlogtarget, :dtensorlogtarget)
+  fields = (:gradloglikelihood, :gradlogprior, :gradlogtarget, :tensorlogtarget, :dtensorlogtarget)
   ContinuousMultivariateParameterState(
     value, 
     Bool[haskey(monitor, fields[i]) ? monitor[fields[i]] : false for i in 1:5]
   )
 end
 
-function ContinuousMultivariateParameterState{N<:FloatingPoint}(
-  ::Type{N}=Float64,
-  size::Int=0,
-  monitor::Vector{Bool}=fill(false, 5)
-  )
+ContinuousMultivariateParameterState{N<:FloatingPoint}(::Type{N}, size::Int=0, monitor::Vector{Bool}=fill(false, 5)) =
   ContinuousMultivariateParameterState(Array(N, size), monitor)
-end
 
-function ContinuousMultivariateParameterState{N<:FloatingPoint}(
-  ::Type{N}=Float64,
-  size::Int=0,
-  monitor::Dict{Symbol, Bool}=Dict{Symbol, Bool}()
-  )
+ContinuousMultivariateParameterState{N<:FloatingPoint}(::Type{N}, size::Int, monitor::Dict{Symbol, Bool}) =
   ContinuousMultivariateParameterState(Array(N, size), monitor)
-end
+
+ContinuousMultivariateParameterState{N<:FloatingPoint}(;
+  value::Vector{N}=Array(N, zero(Int)),
+  loglikelihood::N=convert(N, NaN),
+  logprior::N=convert(N, NaN),
+  logtarget::N=convert(N, NaN),
+  gradloglikelihood::Vector{N}=Array(N, zero(Int)),
+  gradlogprior::Vector{N}=Array(N, zero(Int)),
+  gradlogtarget::Vector{N}=Array(N, zero(Int)),
+  tensorlogtarget::Matrix{N}=Array(N, zero(Int), zero(Int)),
+  dtensorlogtarget::Array{N, 3}=Array(N, zero(Int), zero(Int), zero(Int)),
+  size::Int=length(value),
+  diagnostics::Dict=Dict()
+) =
+  ContinuousMultivariateParameterState{N}(
+    value,
+    loglikelihood,
+    logprior,
+    logtarget,
+    gradloglikelihood,
+    gradlogprior,
+    gradlogtarget,
+    tensorlogtarget,
+    dtensorlogtarget,
+    size,
+    diagnostics
+  )
