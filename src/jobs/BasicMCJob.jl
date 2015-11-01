a5a31682ef7a7769822dff8125e23ca74d11410c
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2015-11-01 22:32:02 +0000

    Code-generating Metropolis iteration (semi-complete but works in a simple setting)

--- src/jobs/BasicMCJob.jl
+++ src/jobs/BasicMCJob.jl
@@ -16,9 +16,10 @@ type BasicMCJob <: MCJob
   plain::Bool # If plain=false then job flow is controlled via tasks, else it is controlled without tasks
   task::Union{Task, Void}
   send::Function
-  receive::Function
-  reset::Function
-  save::Function
+  iterate!::Function
+  receive!::Function
+  reset!::Function
+  save!::Function
   close::Function
   # checkin::Bool # If checkin=true then check validity of job constructors' input arguments, else don't check
 
@@ -53,40 +54,98 @@ type BasicMCJob <: MCJob
     instance.count = 1
 
     instance.plain = plain
+
     if plain
       instance.task = nothing
       instance.send = () -> ()
-      instance.receive = () -> iterate!(
-        instance.vstate,
-        instance.sstate,
-        model.vertices[vindex],
-        vindex,
-        sampler,
-        tuner,
-        range,
-        outopts,
-        instance.count,
-        () -> ()
-      )
-      instance.reset = x::Vector -> reset!(instance.vstate, x, model.vertices[vindex], vindex, sampler)
+      instance.iterate! = eval(codegen_iterate_basic_mcjob(instance, outopts))
+      # instance.receive! = () -> iterate!(
+      #   instance.vstate,
+      #   instance.sstate,
+      #   model.vertices[vindex],
+      #   vindex,
+      #   sampler,
+      #   tuner,
+      #   range,
+      #   outopts,
+      #   instance.count,
+      #   () -> ()
+      # )
+      instance.reset! = x::Vector -> reset!(instance.vstate, x, model.vertices[vindex], vindex, sampler)
     else
       instance.task = Task(() -> initialize_task!(
         instance.vstate, instance.sstate, parameter, vindex, sampler, tuner, range, outopts, count)
       )
       instance.send = produce
-      instance.receive = () -> consume(instance.task)
-      instance.reset = x::Vector -> reset(instance.task, x)
+      instance.iterate! = () -> consume(instance.task)
+      # instance.receive! = () -> consume(instance.task)
+      instance.reset! = x::Vector -> reset(instance.task, x)
     end
 
     if outopts[:destination] == :nstate
-      instance.save = (i::Int) -> instance.output.copy(instance.vstate[vindex], i)
+      instance.save! = (i::Int) -> instance.output.copy(instance.vstate[vindex], i)
       instance.close = () -> ()
     elseif outopts[:destination] == :iostream
-      instance.save = (i::Int) -> instance.output.write(instance.vstate[vindex])
+      instance.save! = (i::Int) -> instance.output.write(instance.vstate[vindex])
       instance.close = () -> close(instance.output)
     end
 
     instance
+  end
+end
+
+function codegen_iterate_basic_mcjob(job::BasicMCJob, outopts::Dict{Symbol, Any})
+  body = []
+
+  if job.tuner.verbose
+    push!(body, :($(job).sstate.tune.proposed += 1))
+  end
+
+  push!(body, :($(job).sstate.pstate.value = $(job).sampler.randproposal($(job).vstate[$(job).vindex].value)))
+  push!(body, :($(job).model.vertices[$(job).vindex].logtarget!($(job).sstate.pstate, $(job).vstate)))
+
+  if job.sampler.symmetric
+    push!(body, :($(job).sstate.ratio = $(job).sstate.pstate.logtarget-$(job).vstate[$(job).vindex].logtarget))
+  else
+    push!(body, :($(job).sstate.ratio = (
+      $(job).sstate.pstate.logtarget
+      +$(job).sampler.logproposal($(job).sstate.pstate.value, $(job).vstate[$(job).vindex].value)
+      -$(job).vstate[$(job).vindex].logtarget
+      -$(job).sampler.logproposal($(job).vstate[$(job).vindex].value, $(job).sstate.pstate.value)
+    )))
+  end
+
+  if job.tuner.verbose
+    push!(body, :(
+      if $(job).sstate.ratio > 0 || ($(job).sstate.ratio > log(rand()))
+        $(job).vstate[$(job).vindex].value = copy($(job).sstate.pstate.value)
+        $(job).vstate[$(job).vindex].logtarget = copy($(job).sstate.pstate.logtarget)
+
+        $(job).sstate.tune.accepted += 1
+      end
+    ))
+
+    #push!(body, :(
+    #  if $(job).count <= $(job).range.burnin && mod($(job).count, $(job).tuner.period) == 0
+    #    tune!($(job).sstate.tune, $(job).tuner)
+    #    println("Burnin iteration $(job.count) of $(job.range.burnin): ", round(100*$(job).sstate.tune.rate, 2), " % acceptance rate")
+    #  end
+    #end
+  else
+    push!(body, :(
+      if $(job).sstate.ratio > 0 || ($(job).sstate.ratio > log(rand()))
+        $(job).vstate[$(job).vindex].value = copy($(job).sstate.pstate.value)
+        $(job).vstate[$(job).vindex].logtarget = copy($(job).sstate.pstate.logtarget)
+      end
+    ))
+  end
+
+  @gensym iterate_basic_mcjob
+
+  quote
+    function $iterate_basic_mcjob()
+      $(body...)
+    end
   end
 end
 
@@ -95,16 +154,16 @@ end
 
 function Base.run(job::BasicMCJob)
   for i in 1:job.range.nsteps
-    job.receive()
-    
+    job.iterate!()
+
     if in(i, job.range.postrange)
-      job.save(job.count)
-      
+      job.save!(job.count)
+
       job.count += 1
     end
   end
 
   job.close()
-  
+
   job.output
 end
